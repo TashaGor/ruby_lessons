eb35a10701d1af284c36b1cecac7e561df35b07b
Tasha Gor <nebesnii_veter@mail.ru>
2015-09-24 12:06:41 +0100

    triangle_pifagor

--- lesson_3/triangle.rb
+++ lesson_3/triangle.rb
@@ -11,4 +11,17 @@ elsif a == b || b == c || c == a
 else
 	puts "Треугольник с разными сторонами"
 end
-		
+
+if a > b && a > c
+  gyp, cat1, cat2  = a, b, c
+elsif b > a && b > c
+  gyp, cat1, cat2  = b, a, c	
+elsif c > a && c > b
+  gyp, cat1, cat2  = c, b, a
+end
+
+if gyp && (gyp**2 == cat1**2 + cat2**2)
+	puts "Треугольник является прямоугольным"
+else 
+	puts "Треугольник не прямоугольный"
+end
