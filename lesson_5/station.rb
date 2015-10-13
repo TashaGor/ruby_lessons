0038e03370042c06484aba51b365d638f2b65959
Tasha Gor <nebesnii_veter@mail.ru>
2015-10-13 14:58:18 +0100

    full station

--- lesson_5/station.rb
+++ lesson_5/station.rb
@@ -1,24 +1,31 @@
 class Station
+	attr_reader :trains, :name
+
 	def initialize(name)
 		@name = name
 		@trains = []
 	end
 
-	def trains
-		@trains
-	end
-
 	def train_arrival(train)
-		@trains.push train.num
+		@trains.push train
 	end
 
 	def train_departure(train)
-		@trains.delete(train.num)
+		@trains.delete(train)
+	end
+
+	def trains_list_type
+		trains_list = {}
+    @trains.each do |train|
+      trains_list[train.type] ||= 0
+      trains_list[train.type] += 1
+    end
+    trains_list
 	end
 end
 
 class Train
-	attr_reader :num, :speed
+	attr_reader :num, :speed, :type
 
 	def initialize(num, type, wagon_count)
 		@num = num
@@ -54,23 +61,81 @@ class Train
 	def wagon_count_down
 		self.wagon_count = @wagon_count - 1 if @speed == 0
 	end
+
+  def add_route(route)
+    @station_list = route.station_list
+    @current_station = 0
+  end
+
+  def go_next_station
+    @current_station += 1 if @current_station < @station_list.count
+  end
+
+  def go_prev_station
+    @current_station -= 1  if @current_station > 0
+  end
+
+  def current_station
+    @station_list[@current_station]
+  end
+
+  def next_station
+    @station_list[@current_station+1]
+  end
+
+  def prev_station
+    @station_list[@current_station-1]
+  end
 end
 
+class Route
+  attr_reader :station_list
 
-=begin
+  def initialize(first_station, last_station)
+    @station_list = []
+    @station_list[0] = first_station
+    @station_list << last_station
+  end
+
+  def insert_station(station, index)
+    @station_list.insert(index, station)
+  end
+
+  def remove_station(index)
+    @station_list.delete_at(index)
+  end
+
+  def list
+    @station_list.each do |station|
+      p station.name
+    end
+  end
+end
+
 station1 = Station.new("Суконная Слобода")
+station2 = Station.new("Кремлевская")
 train1 = Train.new(1, "грузовой", 6)
-
 train2 = Train.new(2, "пассажирский", 2)
-train2.wagon_count_down
-train2.speed = 120
-p train2.speed
-
-train7 = Train.new(7, "грузовой", 9)
+train3 = Train.new(23, "пассажирский", 2)
+train4 = Train.new(3, "пустой", 0)
 
 station1.train_arrival train1
 station1.train_arrival train2
-p station1.trains
+station1.train_arrival train3
+station1.train_arrival train4
+p station1.trains_list_type
+
 station1.train_departure train1
-p station1.trains=end
+p station1.trains_list_type
+
+route1 = Route.new(station1, station2)
+station3 = Station.new("Приволжская")
+route1.insert_station(station3, 1)
+route1.list
+
+train1.add_route(route1)
+train1.go_next_station
+p train1.current_station
+p train1.prev_station
+p train1.next_station
 
